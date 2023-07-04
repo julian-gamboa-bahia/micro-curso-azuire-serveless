79519d6b5ab1d8ff85ab94330bba3fe2b2a9c455
julian-gamboa-ensino <julian.gamboa.ms.2021@gmail.com>
2023-07-03 23:13:02 -0300

    first commit

---
+++ resp/shell.sh
@@ -0,0 +1,10 @@
+ az group create --name AzureFunctionsQuickstart-rg --location westus
+ az storage account create --name apagajulho --location westus --resource-group AzureFunctionsQuickstart-rg --sku Standard_LRS --allow-blob-public-access false
+
+ az functionapp create --resource-group AzureFunctionsQuickstart-rg --consumption-plan-location westus --runtime dotnet-isolated --functions-version 4 --name appjulioapagar --storage-account apagajulho
+
+func azure functionapp publish appjulioapagar
+
+exit
+
+az group delete --name AzureFunctionsQuickstart-rg
